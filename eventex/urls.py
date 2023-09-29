b15633960e1e4eecface77de2b3efcd12afe7e63
DavidSilveira80 <davidpscontato@gmail.com>
2023-09-29 16:44:42 -0300

    Tela de inscrição.

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 import eventex.core.views
+from eventex.subscriptions.views import subscribe
 
 urlpatterns = [
     path('', eventex.core.views.home),
     path('admin/', admin.site.urls),
-    path('existe/', eventex.core.views.existe, name='existe')
+    path('existe/', eventex.core.views.existe, name='existe'),
+    path('inscricao/', subscribe),
 
 ]
